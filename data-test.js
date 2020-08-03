d340f4f5e22a2eee3d0e4f3f83397d89d40043c7
felix tineo <ftandcompany@gmail.com>
2020-08-03 17:17:05 -0400

    initial commit

---
+++ data-test.js
@@ -0,0 +1,152 @@
+export default {
+  webOffice:{
+    primaryColor: "#FFA600",
+    secondaryColor: "#FFFFFF",
+    textColor: "#212121",
+    dailyRate:{
+      UF: 75875987,
+      UTM: 75875987,
+      DOLAR: 75875987,
+    },
+    logo: '/logo.png',
+    favicon: '/favicon',
+    phone: '+56 9 4581 8490',
+    sections:{
+      hero:{
+        mainText: 'Tenemos propiedades exclusivas pensadas para ti',
+      },
+      properties: {
+        propertyList:[
+          {
+            mainImage: '/properties/image-1.jpg',
+            title: "Casa en lo Barnechea",
+            currency: 'UF',
+            value: 75000,
+            operation: "Venta",
+            code: "345678",
+            ubication:{
+              address: "Valle del Monasterio / manquele oriente",
+            },
+            characteristics:[
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Habitaciones",
+                value: 4
+              },
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Sup. Total",
+                value: 1000
+              },
+            ]
+          },
+          {
+            mainImage: '/properties/image-2.jpg',
+            title: "Casa en Vitcura",
+            currency: 'UF',
+            value: 75000,
+            operation: "Venta",
+            code: "345678",
+            ubication:{
+              address: "Lo Curro",
+            },
+            characteristics:[
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Habitaciones",
+                value: 8
+              },
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Sup. Total",
+                value: 1000
+              },
+            ]
+          },
+          {
+            mainImage: '/properties/image-3.jpg',
+            title: "Casa en Las Condes",
+            currency: 'UF',
+            value: 75000,
+            operation: "Venta",
+            code: "345678",
+            ubication:{
+              address: "Malaga",
+            },
+            characteristics:[
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Habitaciones",
+                value: 6
+              },
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Sup. Total",
+                value: 1000
+              },
+            ]
+          },
+          {
+            mainImage: '/properties/image-4.jpg',
+            title: "Casa en lo Barnechea",
+            currency: 'UF',
+            value: 75000,
+            operation: "Venta",
+            code: "345678",
+            ubication:{
+              address: "Rosario / Casorra Alzamona",
+            },
+            characteristics:[
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Habitaciones",
+                value: 6
+              },
+              {
+                id: "5f0591b437096b0008b019d5",
+                name: "Sup. Total",
+                value: 1000
+              },
+            ]
+          },                    
+        ],
+        footer:{
+          backgroundImage: '',
+          backgroundColor: '#ffffff'
+        }
+      },
+      about:{
+        title: "Somos una empresa con mas de 20 años en el rubro del corretaje inmobiliario",
+        p: "Esto nos permite entender mejor que nadie las necesidades de nuestros clientes, por eso ponemos el foco en que la experienca de compra o arriendo sea única",
+      },
+      service:[
+        {
+          title: 'Compras',
+          p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium fringilla ex at hendrerit',
+        },
+        {
+          title: 'Ventas',
+          p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium fringilla ex at hendrerit',
+        },
+        {
+          title: 'Arriendo',
+          p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium fringilla ex at hendrerit',
+        },                
+      ],
+      reviews:[
+        {
+          p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate sapien at nisi volutpat dictum. Maecenas vel lorem tellus. Sed sit amet sollicitudin nisi. Sed consectetur eget orci sit amet condimentum.',
+          client:'Adrian Carcamo - Clasihome'
+        },
+        {
+          p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate sapien at nisi volutpat dictum. Maecenas vel lorem tellus. Sed sit amet sollicitudin nisi. Sed consectetur eget orci sit amet condimentum.',
+          client:'Elon musk - Space-x'
+        },
+        {
+          p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate sapien at nisi volutpat dictum. Maecenas vel lorem tellus. Sed sit amet sollicitudin nisi. Sed consectetur eget orci sit amet condimentum.',
+          client:'Montgomery burns - Planta nuclear de springfield'
+        },        
+      ]
+    }
+  }
+}
